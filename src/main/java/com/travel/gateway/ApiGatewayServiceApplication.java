18d6d70c1d3b0591e3a7c69065e3f3c5ba0bcc53
Burton Arceneaux <btarceneaux@gmail.com>
2023-10-08 07:56:44 -0700

    CORS is now working on gateway.

--- src/main/java/com/travel/gateway/ApiGatewayServiceApplication.java
+++ src/main/java/com/travel/gateway/ApiGatewayServiceApplication.java
@@ -3,10 +3,27 @@ package com.travel.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ApiGatewayServiceApplication {
+
+	@Bean
+	public CorsWebFilter corsWebFilter() {
+		CorsConfiguration corsConfig = new CorsConfiguration();
+		corsConfig.addAllowedOrigin("*");
+		corsConfig.addAllowedMethod("*");
+		corsConfig.addAllowedHeader("*");
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig);
+
+		return new CorsWebFilter(source);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayServiceApplication.class, args);
